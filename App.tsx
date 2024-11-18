cf245503dd878b96acc8bdf281c54ad9182b8f4d
Eduardo Fernando Farfus <eduardo.farfus@angellira.com.br>
2024-11-18 17:45:41 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,98 +1,40 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+import params from './src/params'
+import Field from './src/components/Field'
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={styles.sectionContainer}>
+      <Text style={styles.sectionTitle}>Iniciando o Mines!</Text>
+      <Text style={styles.sectionTitle}>Tamanho da grade: {params.getRowsAmount()}x{params.getCollumnsAmount()}</Text>
+      <Field></Field>
+      <Field opened></Field>
+      <Field opened nearMines={1}></Field>
+      <Field opened nearMines={2}></Field>
+      <Field opened nearMines={3}></Field>
+      <Field opened nearMines={4}></Field>
+      <Field opened nearMines={5}></Field>
+      <Field opened nearMines={6}></Field>
+      <Field opened nearMines={7}></Field>
+      <Field opened nearMines={8}></Field>
+      <Field mined ></Field>
+      <Field mined opened></Field>
+      <Field mined opened exploded></Field>
+      <Field flagged></Field>
+      <Field flagged opened></Field>
+
+
+    </View>
   );
 }
 
